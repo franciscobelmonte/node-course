bad0a72fd220473a3105a6788e4dac48693b238c
Francisco Belmonte Ruiz <francisco.belmonte.ruiz@gmail.com>
2019-02-04 23:47:37 +0100

    Initialized project to develop a todo console app in Node

---
+++ 04-todo/app.js
@@ -0,0 +1,24 @@
+const argv = require('yargs').argv;
+
+console.log(argv);
+
+let command = argv._[0];
+
+switch (command) {
+    case 'create':
+
+        break;
+
+    case 'list':
+
+        break;
+
+    case 'update':
+
+        break;
+
+    default:
+        console.error('Command unknown');
+        return;
+        break;
+}
