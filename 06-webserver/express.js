7666c6def882d65d3bc948b3805e6ac59a281f47
Francisco Belmonte Ruiz <francisco.belmonte.ruiz@gmail.com>
2019-02-10 19:19:23 +0100

    Created http server using express

---
+++ 06-webserver/express.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    let json = {
+        name: 'Francisco',
+        age: 31,
+        url: req.url,
+    }
+    res.send(json);
+});
+
+app.get('/hello', (req, res) => {
+    res.send('Hello world!');
+});
+
+app.listen(3000, () => {
+    console.log('Listening requests in port 3000.');
+});
