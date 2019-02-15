867ae19fed5f94a411c9a4ab17388612bb80521d
Francisco Belmonte Ruiz <fbelmonte@rimontgo.es>
2019-02-15 16:37:52 +0100

    Created user schema

---
+++ 07-restserver/server/models/user.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+let Schema = mongoose.Schema;
+
+let user = new Schema({
+    name: {
+        type: String,
+        required: [true, 'Name is required']
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required']
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required']
+    },
+    img: {
+        type: String,
+        required: false
+    },
+    role: {
+        type: String,
+        default: 'USER_ROLE'
+    },
+    status: {
+        type: Boolean,
+        default: true
+    },
+    google: {
+        type: Boolean,
+        default: false
+    }
+});
+
+module.exports = mongoose.model('User', user);
