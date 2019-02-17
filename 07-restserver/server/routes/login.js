f93e95ae2262edaf4bb61234fdf471723e7533f8
Francisco Belmonte Ruiz <francisco.belmonte.ruiz@gmail.com>
2019-02-17 19:38:01 +0100

    Created route to login user detecting if password is valid

--- 07-restserver/server/routes/login.js
+++ 07-restserver/server/routes/login.js
@@ -5,9 +5,41 @@ const bcrypt = require('bcrypt');
 const User = require('../models/user');
 
 app.post('/login', (req, res) => {
-    res.json({
-        error: false
+    let body = req.body;
+
+    User.findOne({ email: body.email }, (err, userDB) => {
+        if (err) {
+            res.status(500).json({
+                error: err
+            });
+            return;
+        }
+
+        if (!userDB) {
+            res.status(400).json({
+                error: {
+                    message: '(User) or password invalid'
+                }
+            });
+            return;
+        }
+
+        if (!bcrypt.compareSync(body.password, userDB.password)) {
+            res.status(400).json({
+                error: {
+                    message: 'User or (password) invalid'
+                }
+            });
+            return;
+        }
+
+        res.json({
+            error: false,
+            user: userDB,
+            token: '123'
+        });
     });
+
 });
 
 module.exports = app;
