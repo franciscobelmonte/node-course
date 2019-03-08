32c978318fff8033e853d7eadc51ca3055094b43
Francisco Belmonte Ruiz <francisco.belmonte.ruiz@gmail.com>
2019-03-08 15:24:56 +0100

    Connected and disconnected via socket

--- 08-sockets/server/server.js
+++ 08-sockets/server/server.js
@@ -1,13 +1,29 @@
+const path = require('path');
+
+const http = require('http');
+const socketIO = require('socket.io');
+
 const express = require('express');
 const app = express();
 
-const path = require('path');
+let server = http.createServer(app);
 
 const port = process.env.PORT || 3000;
 
 app.use(express.static(path.resolve(__dirname, '../public')));
 
-app.listen(port, (err) => {
+let io = socketIO(server);
+
+io.on('connection', (client) => {
+    console.log('User connected');
+
+    client.on('disconnect', (client) => {
+        console.log('User disconnected');
+    });
+});
+
+
+server.listen(port, (err) => {
     if (err) throw new Error(err);
 
     console.log(`Server running in port ${port}`);
