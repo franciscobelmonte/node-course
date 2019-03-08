62dc7cb52a0fe0aa5b1c22c8ef6beab787c32aaa
Francisco Belmonte Ruiz <francisco.belmonte.ruiz@gmail.com>
2019-03-08 17:44:58 +0100

    Attend ticket in server

--- 09-sockets-tickets/server/classes/ticket-control.js
+++ 09-sockets-tickets/server/classes/ticket-control.js
@@ -1,15 +1,26 @@
 const fs = require('fs');
 
+class Ticket {
+    constructor(number, desktop) {
+        this.number = number;
+        this.desktop = desktop;
+    }
+}
+
 class TicketControl {
     constructor() {
         this.last = 0;
         this.today = new Date().getDate();
+        this.tickets = [];
+        this.lastTickets = [];
 
         let data = require('../data/data.json');
         console.log(data);
 
         if (data.hoy === this.today) {
             this.last = data.last;
+            this.tickets = data.tickets;
+            this.lastTickets = data.lastTickets;
         } else {
             this.reset();
         }
@@ -17,6 +28,9 @@ class TicketControl {
 
     next() {
         this.last++;
+        let ticket = new Ticket(this.last, null);
+        this.tickets.push(ticket);
+
         this.saveFile();
 
         return `Ticket ${this.last}`;
@@ -26,19 +40,38 @@ class TicketControl {
         return `Ticket ${this.last}`;
     }
 
-    reset() {
-        let json = {
-            last: this.last,
-            today: this.today
-        };
+    attendTicket(desktop) {
+        if (this.tickets.length === 0) {
+            return 'No tickets';
+        }
 
-        fs.writeFileSync('./server/data/data.json', JSON.stringify(json));
+        let number = this.tickets[0].number;
+        this.tickets.shift();
+
+        let ticket = new Ticket(number, desktop);
+
+        this.lastTickets.unshift(ticket);
+        if (this.lastTickets.length > 4) {
+            this.lastTickets.splice(-1, 1);
+        }
+
+        this.saveFile();
+        return ticket;
+    }
+
+    reset() {
+        this.last = 0;
+        this.tickets = [];
+        this.lastTickets = [];
+        this.saveFile();
     }
 
     saveFile() {
         let json = {
             last: this.last,
-            today: this.today
+            today: this.today,
+            tickets: this.tickets,
+            lastTickets: this.lastTickets
         };
 
         fs.writeFileSync('./server/data/data.json', JSON.stringify(json));
