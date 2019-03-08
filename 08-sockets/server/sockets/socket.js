1cce1e3844d177cd360297d27d3a9caa3e8d80f0
Francisco Belmonte Ruiz <francisco.belmonte.ruiz@gmail.com>
2019-03-08 15:48:59 +0100

    Broadcast event to all clients

--- 08-sockets/server/sockets/socket.js
+++ 08-sockets/server/sockets/socket.js
@@ -15,14 +15,16 @@ io.on('connection', (client) => {
     // Listen event from client
     client.on('sendMessage', (message, callback) => {
         console.log(message);
-        if (message.user) {
-            callback({
-                error: false
-            });
-        } else {
-            callback({
-                error: true
-            });
-        }
+
+        client.broadcast.emit('sendMessage', message);
+        // if (message.user) {
+        //     callback({
+        //         error: false
+        //     });
+        // } else {
+        //     callback({
+        //         error: true
+        //     });
+        // }
     });
 });
