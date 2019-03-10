6e738f9a46a380dc05db71d3cb9fa2ea170d43fb
Francisco Belmonte Ruiz <francisco.belmonte.ruiz@gmail.com>
2019-03-10 18:34:36 +0100

    Used Router Express

--- 11-typescript-mysql/src/index.ts
+++ 11-typescript-mysql/src/index.ts
@@ -1,8 +1,11 @@
 console.log('Typescript code');
 
 import Server from './server/server';
+import router from './router/router';
 
 const server = Server.init(3000);
+server.app.use(router);
+
 server.start(() => {
     console.log('Server running...');
 })
